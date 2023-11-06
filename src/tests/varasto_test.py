547f3b1b92ab2a076e7e47aef4675187ab837fd6
fallmx <31561277+fallmx@users.noreply.github.com>
2023-11-06 22:28:16 +0200

    Add unit tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_virheellinen_tilavuus_nollataan(self):
+        neg_tilavuus = Varasto(-5)
+
+        self.assertAlmostEqual(neg_tilavuus.tilavuus, 0)
+
+    def test_virheellinen_alkusaldo_nollataan(self):
+        neg_alkusaldo = Varasto(5, -5)
+
+        self.assertAlmostEqual(neg_alkusaldo.saldo, 0)
+
+    def test_alkusaldo_ei_ylita_tilavuutta(self):
+        iso_alkusaldo = Varasto(5, 10)
+
+        self.assertAlmostEqual(iso_alkusaldo.saldo, 5)
+
+    def test_negatiivinen_lisays_ei_sallittu(self):
+        self.varasto.lisaa_varastoon(-5)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_iso_lisays_tayttaa_tilavuuden(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_negatiivinen_otto_nollataan(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-5), 0.0)
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+    def test_iso_otto_tyhjentaa_saldon(self):
+        self.varasto.lisaa_varastoon(5)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(7), 5.0)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+    def test_tulostus_toimii(self):
+        self.varasto.lisaa_varastoon(7)
+        
+        self.assertAlmostEqual(str(self.varasto), "saldo = 7, vielä tilaa 3")
