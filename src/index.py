79c2b04ce5944c20d6fc37f4b5645167ba9858e6
fallmx <31561277+fallmx@users.noreply.github.com>
2023-11-13 22:24:43 +0200

    Viikko 2 tehtävä 3

--- src/index.py
+++ src/index.py
@@ -1,19 +1,13 @@
 from varasto import Varasto
 # test
 
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
-
+def print_olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def print_mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,7 +16,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
-    print("Virhetilanteita:")
+def print_virhe_class():
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
     print(huono)
@@ -31,28 +25,51 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def print_add_extra(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
 
+def print_add_negative(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def print_take_extra(olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
 
+def print_take_negative(mehua):
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
 
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    print("Luonnin jälkeen:")
+    print(f"Mehuvarasto: {mehua}")
+    print(f"Olutvarasto: {olutta}")
+
+    print_olut_getterit(olutta)
+    print_mehu_setterit(mehua)
+
+    print("Virhetilanteita:")
+
+    print_virhe_class()
+    print_add_extra(olutta)
+    print_add_negative(mehua)
+    print_take_extra(olutta)
+    print_take_negative(mehua)
 
 if __name__ == "__main__":
     main()
